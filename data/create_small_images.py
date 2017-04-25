8d12b5b5774283b5420a660e3ae90e009f9d9928
Mårten Nilsson <nilssonmartenx@gmail.com>
2017-04-25 13:01:18 +0200

    Script for resizing data

---
+++ data/create_small_images.py
@@ -0,0 +1,19 @@
+import PIL
+import glob
+import sys
+import os
+
+place = os.getcwd()
+lfw_path = place + "/lfw-deepfunneled/"
+out_path = place + "/small_pics/"
+
+k = 0
+n = 1000
+for path, dirs, files in os.walk(lfw_path):
+    if len(files) > 0 and k < n:
+        for i in files:
+            image_path = path + "/" + i
+            os.system("convert -resize 64x64 {0} {1}out{2}.jpg".format(image_path, out_path, k))
+
+            k += 1
+
