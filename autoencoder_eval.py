cb3228e15c4d7dbc640c5a564817da8e63b205c4
Mårten Nilsson <nilssonmartenx@gmail.com>
2017-04-25 18:11:27 +0200

    jpeg input to evaluation implemented

--- autoencoder_eval.py
+++ autoencoder_eval.py
@@ -1,13 +1,13 @@
-
 # Playground to evaluate without training the encoder
 import tensorflow as tf
 import numpy as np
 import scipy.ndimage as image
 import matplotlib.pyplot as plt
 from autoencoder_graph import create_autoencoder
+import sys
+import os
 
 np_data = np.load("data/numpy_data.npy")
-
 
 # Get graph
 create_autoencoder()
@@ -23,9 +23,24 @@ print()
 print()
 print("------------------------------")
 
+# If we have command line arguments, do fun stuff!
+if len(sys.argv) > 0:
+    url = sys.argv[1]
+    os.system("convert -resize 64x64\! {0} /tmp/das_pic.jpg".format(url))
+    pic = image.imread("/tmp/das_pic.jpg")
+
+    plt.figure()
+    plt.imshow(pic/255)
+
+    pic_in = pic.reshape([1, 64, 64, 3])
+    out = sess.run("raw_out:0", feed_dict={"raw_data:0": pic_in})
+    plt.figure()
+    pueh = (out[0] - np.min(out[0])) / (np.max(out[0]) - np.min(out[0]))
+    plt.imshow(pueh)
+
 # Test the model
-image = np_data[:8]
-out = sess.run("raw_out:0", feed_dict={"raw_data:0": image})
+images = np_data[:8]
+out = sess.run("raw_out:0", feed_dict={"raw_data:0": images})
 
 # Plot some samples
 
